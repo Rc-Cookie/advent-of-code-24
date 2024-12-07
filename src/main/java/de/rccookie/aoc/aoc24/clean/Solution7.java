d0579804f252c15ebcea9b56a2a62c7311096411
Rc-Cookie <leomue@mail.de>
2024-12-07 14:00:33 +0100

    Add lower bounds optimization to clean solution

--- src/main/java/de/rccookie/aoc/aoc24/clean/Solution7.java
+++ src/main/java/de/rccookie/aoc/aoc24/clean/Solution7.java
@@ -8,7 +8,15 @@ public class Solution7 extends FastSolution {
     public Object task1() {
         return sum(l -> {
             long[] nums = parseLongs(l);
-            return computablePlusMinus(nums[0], nums[1], nums, 2) ? nums[0] : 0;
+
+            long[] lowerBounds = new long[nums.length];
+            long maxIncrement = 1;
+            for(int i=nums.length-1; i>=2; i--) {
+                maxIncrement *= nums[i];
+                lowerBounds[i] = (nums[0]-1) / maxIncrement;
+            }
+
+            return computablePlusMinus(nums[0], nums[1], nums, lowerBounds, 2) ? nums[0] : 0;
         });
     }
 
@@ -16,34 +24,43 @@ public class Solution7 extends FastSolution {
     public Object task2() {
         return sum(l -> {
             long[] nums = parseLongs(l);
+
             int[] magnitudes = new int[nums.length];
             for(int i=2; i<magnitudes.length; i++) {
                 magnitudes[i] = 10;
                 while(magnitudes[i] <= nums[i])
                     magnitudes[i] *= 10;
             }
-            return computablePlusMinusConcat(nums[0], nums[1], nums, magnitudes, 2) ? nums[0] : 0;
+
+            long[] lowerBounds = new long[nums.length];
+            long maxIncrement = 1;
+            for(int i=nums.length-1; i>=2; i--) {
+                maxIncrement *= magnitudes[i];
+                lowerBounds[i] = nums[0] / maxIncrement;
+            }
+
+            return computablePlusMinusConcat(nums[0], nums[1], nums, magnitudes, lowerBounds, 2) ? nums[0] : 0;
         });
     }
 
-    private boolean computablePlusMinus(long target, long current, long[] nums, int nextIndex) {
+    private boolean computablePlusMinus(long target, long current, long[] nums, long[] lowerBounds, int nextIndex) {
         if(current == target)
             return true;
-        if(current > target || nextIndex >= nums.length)
+        if(current > target || nextIndex >= nums.length || current < lowerBounds[nextIndex])
             return false;
         long x = nums[nextIndex];
-        return computablePlusMinus(target, current + x, nums, nextIndex + 1) ||
-                computablePlusMinus(target, current * x, nums, nextIndex + 1);
+        return computablePlusMinus(target, current + x, nums, lowerBounds, nextIndex + 1) ||
+                computablePlusMinus(target, current * x, nums, lowerBounds, nextIndex + 1);
     }
 
-    private boolean computablePlusMinusConcat(long target, long current, long[] nums, int[] magnitudes, int nextIndex) {
+    private boolean computablePlusMinusConcat(long target, long current, long[] nums, int[] magnitudes, long[] lowerBounds, int nextIndex) {
         if(current == target)
             return true;
-        if(current > target || nextIndex >= nums.length)
+        if(current > target || nextIndex >= nums.length || current < lowerBounds[nextIndex])
             return false;
         long x = nums[nextIndex];
-        return computablePlusMinusConcat(target, current + x, nums, magnitudes, nextIndex + 1) ||
-                computablePlusMinusConcat(target, current * x, nums, magnitudes, nextIndex + 1) ||
-                computablePlusMinusConcat(target, current * magnitudes[nextIndex] + x, nums, magnitudes, nextIndex + 1);
+        return computablePlusMinusConcat(target, current + x, nums, magnitudes, lowerBounds, nextIndex + 1) ||
+                computablePlusMinusConcat(target, current * x, nums, magnitudes, lowerBounds, nextIndex + 1) ||
+                computablePlusMinusConcat(target, current * magnitudes[nextIndex] + x, nums, magnitudes, lowerBounds, nextIndex + 1);
     }
 }
