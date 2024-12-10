777ac1d84b8aa0f42d79dd8b948a8b6fc2123796
Rc-Cookie <leomue@mail.de>
2024-12-10 01:16:57 +0100

    More optimization

--- src/main/java/de/rccookie/aoc/aoc24/Solution9.java
+++ src/main/java/de/rccookie/aoc/aoc24/Solution9.java
@@ -29,158 +29,90 @@ public class Solution9 extends FastSolution {
         return checksum;
     }
 
-    private int loadFS(int[] fs) {
-        int len = 0;
-        for(int i=0, id=0; i<chars.length; i++, id++) {
-            int size = chars[i] - '0';
-            for(int j=0; j<size; j++)
-                fs[len++] = id;
-            if(++i == chars.length)
-                break;
-            size = chars[i] - '0';
-            for(int j=0; j<size; j++)
-                fs[len++] = -1;
-        }
-        return len;
-    }
-
-    private int[] loadFS() {
-        int total = 0;
-        int len = chars.length - 1;
-        for(int i=0; i<len; i++)
-            total += chars[i];
-        int[] fs = new int[total - len * '0'];
-
-        int fsIndex = 0;
-        for(int i=0, id=0; i<len; i++, id++) {
-            int size = chars[i] - '0';
-            for(int j=0; j<size; j++)
-                fs[fsIndex++] = id;
-            size = chars[++i] - '0';
-            for(int j=0; j<size; j++)
-                fs[fsIndex++] = -1;
-        }
-        return fs;
-    }
-
     @Override
     public Object task2() {
 
-//        int[] fs = loadFS();
-//        int len = fs.length;
+        final char[] chars = this.chars;
+        final int len = chars.length - 1;
+        final int gapCount = len / 2, fileCount = gapCount + 1;
+
+        int i, fsIndex, size;
+        IntHeap h;
 
 
-        int[] gapCursors = new int[10];
-        for(int i=1; i<chars.length-1; i+=2)
-            gapCursors[chars[i] - '0']++;
+        final int[] gapCounts = new int[10];
+        for(i=1; i<len; i+=2)
+            gapCounts[chars[i] - '0']++;
 
-//        int[] gapStarts = new int[(chars.length-1)/2];
-//        byte[] gapSizes = new byte[gapStarts.length];
-        int[] fileStarts = new int[(chars.length+1)/2];
+        IntHeap[] gaps = new IntHeap[9];
+        for(i=0; i<9; i++)
+            gaps[i] = new IntHeap(gapCounts[i+1] + 1);
 
-        IntHeap[] sortedGapStarts = new IntHeap[9];
-        for(int i=0; i<9; i++)
-            sortedGapStarts[i] = new IntHeap(gapCursors[i+1] + 1);
 
-        for(int i=0, fsIndex=0; i<(chars.length-1)/2; i++) {
-            fsIndex += chars[2*i] - '0';
-            int size = chars[2*i+1] - '0';
+        final int[] fileStarts = new int[fileCount];
+
+        for(i=0, fsIndex=0; i<gapCount; i++) {
+            int i2 = i << 1;
+            fsIndex += chars[i2] - '0';
+            size = chars[i2+1] - '0';
             if(size != 0) {
-                IntHeap h = sortedGapStarts[size - 1];
+                h = gaps[size - 1];
                 h.data[h.size++] = fsIndex;
             }
             fsIndex += size;
-//            gapStarts[i] = fsIndex;
-//            gapSizes[i] = (byte) (chars[2*i+1] - '0');
-//            fsIndex += gapSizes[i];
             fileStarts[i+1] = fsIndex;
         }
 
-//        fileStarts[fileStarts.length-1] = gapStarts[gapStarts.length-1] + gapSizes[gapSizes.length-1];
-
-//        int firstGap = 0;
-//        while(gapSizes[firstGap] == 0)
-//            firstGap++;
         int firstGap = Integer.MAX_VALUE;
-        for(int i=0; i<sortedGapStarts.length; i++)
-            if(!sortedGapStarts[i].isEmpty() && firstGap > sortedGapStarts[i].peek())
-                firstGap = sortedGapStarts[i].peek();
-
-//        fs = new int[fs.length];
-//        Arrays.fill(fs, -1);
+        for(i=0; i<9; i++) {
+            h = gaps[i];
+            if(h.size != 0 && firstGap > h.data[0])
+                firstGap = h.data[0];
+        }
 
         long checksum = 0;
-        int file = (chars.length - 1) / 2;
-        for(; firstGap < fileStarts[file]; file--) {
-            byte size = (byte) (chars[2*file] - '0');
-            int gap = Integer.MAX_VALUE;
-            int gapSizeM1 = 0;
 
-            for(int s=size-1; s<9; s++)
-                if(!sortedGapStarts[s].isEmpty() && sortedGapStarts[s].peek() < gap)
-                    gap = sortedGapStarts[gapSizeM1 = s].peek();
+        int file = fileCount - 1;
+        for(int fileStart, gap, gapSizeM1 = 0, s; firstGap < (fileStart = fileStarts[file]); file--) {
+            size = chars[file << 1] - '0';
+            gap = Integer.MAX_VALUE;
 
-            if(gap != Integer.MAX_VALUE && gap < fileStarts[file]) {
-                int gapStart = sortedGapStarts[gapSizeM1].dequeue();
-                for(int j=0; j<size; j++) {
-                    checksum += (long) file * (gapStart + j);
-//                    fs[gapStart + j] = file;
+            for(s=size-1; s<9; s++) {
+                h = gaps[s];
+                if(h.size != 0 && h.data[0] < gap) {
+                    gap = h.data[0];
+                    gapSizeM1 = s;
                 }
+            }
+
+            if(gap < fileStart) {
+                gaps[gapSizeM1].dequeue();
+                // Sum(j=0->size) (file * (gap + j))
+                checksum += file * ((size * (((long) gap << 1) + size - 1)) >>> 1);
 
                 if(gapSizeM1 != size - 1)
-                    sortedGapStarts[gapSizeM1 - size].enqueue(gapStart + size);
+                    gaps[gapSizeM1 - size].enqueue(gap + size);
 
-                if(gapStart == firstGap) {
+                if(gap == firstGap) {
                     firstGap = Integer.MAX_VALUE;
-                    for(int i=0; i<sortedGapStarts.length; i++)
-                        if(!sortedGapStarts[i].isEmpty() && firstGap > sortedGapStarts[i].peek())
-                            firstGap = sortedGapStarts[i].peek();
+                    for(i=0; i<9; i++) {
+                        h = gaps[i];
+                        if(h.size != 0 && firstGap > h.data[0])
+                            firstGap = h.data[0];
+                    }
                 }
             }
-
-//            for(int i=firstGap; i<file; i++) {
-//                if(gapSizes[i] >= size) {
-//                    gap = i;
-//                    break;
-//                }
-//            }
-//
-//            if(gap != Integer.MAX_VALUE) {
-//                for(int j=0; j<size; j++) {
-//                    checksum += (long) file * (gapStarts[gap] + j);
-////                    fs[(gaps[gap] & 0xFFFF) + j] = file;
-//                }
-//                gapStarts[gap] += size;
-//                gapSizes[gap] -= size;
-//                while(gapSizes[firstGap] == 0)
-//                    firstGap++;
-//            }
             else {
-                for (int j = 0; j < size; j++) {
-                    checksum += (long) file * (fileStarts[file] + j);
-//                    fs[fileStarts[file] + j] = file;
-                }
+                // Sum(j=0->size) (file * (fileStart + j))
+                checksum += file * ((size * (((long) fileStart << 1) + size - 1)) >>> 1);
             }
-
-//            int firstGap1 = Integer.MAX_VALUE;
-//            for(int i=0; i<sortedGapStarts.length; i++)
-//                if(!sortedGapStarts[i].isEmpty() && firstGap1 > sortedGapStarts[i].peek())
-//                    firstGap1 = sortedGapStarts[i].peek();
-//            if(firstGap != firstGap1)
-//                throw new AssertionError();
         }
 
         for(; file >= 0; file--) {
-            byte size = (byte) (chars[2*file] - '0');
-            for(int j=0; j<size; j++) {
-                checksum += (long) file * (fileStarts[file] + j);
-//                fs[fileStarts[file] + j] = file;
-            }
+            size = chars[file << 1] - '0';
+            // Sum(j=0->size) (file * (fileStarts[file] + j))
+            checksum += file * ((size * (((long) fileStarts[file] << 1) + size - 1)) >>> 1);
         }
-
-        // 6707707088978
-        // 6421724645083
-//        System.out.println("|"+Arrays.stream(fs).limit(len).mapToObj(i -> i < 0 ? "____" : " ".repeat(4-(""+i).length()) + i).collect(Collectors.joining("|"))+"|");
 
         return checksum;
     }
