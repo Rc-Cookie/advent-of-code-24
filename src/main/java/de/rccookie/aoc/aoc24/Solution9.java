802bff240613173ccfffa3dfa3f15d2b3219ec5c
Rc-Cookie <leomue@mail.de>
2024-12-10 02:02:13 +0100

    Gap search optimization, calculate sums directly rather than with loops

--- src/main/java/de/rccookie/aoc/aoc24/Solution9.java
+++ src/main/java/de/rccookie/aoc/aoc24/Solution9.java
@@ -5,22 +5,25 @@ public class Solution9 extends FastSolution {
     public Object task1() {
 
         int len = 0;
-        int endId = (chars.length-1) / 2;
-        int remainingEnd = chars[2*endId] - '0';
+        int endId = (chars.length-1) >>> 1;
+        int remainingEnd = chars[endId << 1] - '0';
 
         long checksum = 0;
 
         int size, j;
         for(int startId=0; startId<endId; startId++) {
-            size = chars[2*startId] - '0';
-            for(j=0; j<size; j++)
-                checksum += (long) len++ * startId;
-            size = chars[2*startId+1] - '0';
+            size = chars[startId << 1] - '0';
+
+            // Sum(j=0->size) (startId * (len + j))
+            checksum += startId * ((size * (((long) len << 1) + (size-1))) >>> 1);
+            len += size;
+
+            size = chars[(startId<<1)+1] - '0';
             for(j=0; j<size; j++) {
                 checksum += (long) len++ * endId;
                 remainingEnd--;
                 while(remainingEnd <= 0)
-                    remainingEnd = chars[2 * --endId] - '0';
+                    remainingEnd = chars[--endId << 1] - '0';
             }
         }
         for(; remainingEnd > 0; remainingEnd--)
@@ -34,9 +37,9 @@ public class Solution9 extends FastSolution {
 
         final char[] chars = this.chars;
         final int len = chars.length - 1;
-        final int gapCount = len / 2, fileCount = gapCount + 1;
+        final int gapCount = len >>> 1, fileCount = gapCount + 1;
 
-        int i, fsIndex, size;
+        int i, size;
         IntHeap h;
 
 
@@ -45,14 +48,15 @@ public class Solution9 extends FastSolution {
             gapCounts[chars[i] - '0']++;
 
         IntHeap[] gaps = new IntHeap[9];
-        for(i=0; i<9; i++)
-            gaps[i] = new IntHeap(gapCounts[i+1] + 1);
+        for(i=0; i<9; ++i)
+            (gaps[i] = new IntHeap(gapCounts[i+1] + 1)).data[0] = Integer.MAX_VALUE;
 
 
         final int[] fileStarts = new int[fileCount];
 
-        for(i=0, fsIndex=0; i<gapCount; i++) {
-            int i2 = i << 1;
+        i = 0;
+        for(int fsIndex=0, i2; i<gapCount; ++i) {
+            i2 = i << 1;
             fsIndex += chars[i2] - '0';
             size = chars[i2+1] - '0';
             if(size != 0) {
@@ -63,57 +67,61 @@ public class Solution9 extends FastSolution {
             fileStarts[i+1] = fsIndex;
         }
 
-        int firstGap = Integer.MAX_VALUE;
-        for(i=0; i<9; i++) {
-            h = gaps[i];
-            if(h.size != 0 && firstGap > h.data[0])
-                firstGap = h.data[0];
-        }
+        final int[] firstGap = new int[10];
+        final int[] firstGapSize = new int[9];
+        firstGap[9] = Integer.MAX_VALUE;
+        updateFirstGaps(firstGap, firstGapSize, gaps, 8);
 
         long checksum = 0;
 
         int file = fileCount - 1;
-        for(int fileStart, gap, gapSizeM1 = 0, s; firstGap < (fileStart = fileStarts[file]); file--) {
+        for(int fileStart, gap, gapSizeM1, sizeM1; firstGap[0] < (fileStart = fileStarts[file]); --file) {
+
             size = chars[file << 1] - '0';
-            gap = Integer.MAX_VALUE;
+            sizeM1 = size - 1;
 
-            for(s=size-1; s<9; s++) {
-                h = gaps[s];
-                if(h.size != 0 && h.data[0] < gap) {
-                    gap = h.data[0];
-                    gapSizeM1 = s;
-                }
-            }
+            if(firstGap[sizeM1] < fileStart) {
+                gapSizeM1 = firstGapSize[sizeM1];
+                h = gaps[gapSizeM1];
 
-            if(gap < fileStart) {
-                gaps[gapSizeM1].dequeue();
+                gap = h.dequeue();
+                h.data[h.size] = Integer.MAX_VALUE;
                 // Sum(j=0->size) (file * (gap + j))
-                checksum += file * ((size * (((long) gap << 1) + size - 1)) >>> 1);
+                checksum += file * ((size * (((long) gap << 1) + sizeM1)) >>> 1);
 
-                if(gapSizeM1 != size - 1)
+                if(gapSizeM1 != sizeM1)
                     gaps[gapSizeM1 - size].enqueue(gap + size);
 
-                if(gap == firstGap) {
-                    firstGap = Integer.MAX_VALUE;
-                    for(i=0; i<9; i++) {
-                        h = gaps[i];
-                        if(h.size != 0 && firstGap > h.data[0])
-                            firstGap = h.data[0];
-                    }
-                }
+                updateFirstGaps(firstGap, firstGapSize, gaps, gapSizeM1);
             }
             else {
                 // Sum(j=0->size) (file * (fileStart + j))
-                checksum += file * ((size * (((long) fileStart << 1) + size - 1)) >>> 1);
+                checksum += file * ((size * (((long) fileStart << 1) + sizeM1)) >>> 1);
             }
         }
 
-        for(; file >= 0; file--) {
+        for(; file >= 0; --file) {
             size = chars[file << 1] - '0';
             // Sum(j=0->size) (file * (fileStarts[file] + j))
             checksum += file * ((size * (((long) fileStarts[file] << 1) + size - 1)) >>> 1);
         }
 
         return checksum;
+    }
+
+
+    private static void updateFirstGaps(final int[] firstGap, final int[] firstGapSize, final IntHeap[] gaps, int i) {
+        for(int cur, bigger; i>=0; --i) {
+            cur = gaps[i].data[0];
+            bigger = firstGap[i+1];
+            if(cur < bigger) {
+                firstGap[i] = cur;
+                firstGapSize[i] = i;
+            }
+            else {
+                firstGap[i] = bigger;
+                firstGapSize[i] = firstGapSize[i+1];
+            }
+        }
     }
 }
